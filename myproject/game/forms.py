a6aab3fc27e6cde45db7d01847096b9e1f3d246b
Benjamin <benjamin630@icloud.com>
2025-11-17 11:20:45 -0800

    fixed range issue

--- myproject/game/forms.py
+++ myproject/game/forms.py
@@ -10,6 +10,6 @@ class PassRangeParameters(forms.Form):
     multiplication_left_min = forms.IntegerField(initial="2", max_value=1000, required=True, widget=forms.TextInput(attrs={"class": "range-input"}))
     multiplication_left_max = forms.IntegerField(initial="12", max_value=1000, required=True, widget=forms.TextInput(attrs={"class": "range-input"}))
     multiplication_right_min = forms.IntegerField(initial="2", max_value=1000, required=True, widget=forms.TextInput(attrs={"class": "range-input"}))
-    multiplication_right_max = forms.IntegerField(initial="2", max_value=1000, required=True, widget=forms.TextInput(attrs={"class": "range-input"}))
+    multiplication_right_max = forms.IntegerField(initial="100", max_value=1000, required=True, widget=forms.TextInput(attrs={"class": "range-input"}))
 
     duration_selector = forms.IntegerField(required=True)
