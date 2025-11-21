a0d1fa1af0a642fe797e3f7c493eba24357bcaf9
Benjamin <benjamin630@icloud.com>
2025-11-20 20:15:21 -0800

    troubleshooting multiplayer mode issue

--- myproject/myproject/asgi.py
+++ myproject/myproject/asgi.py
@@ -9,15 +9,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+
 from django.core.asgi import get_asgi_application
-from game.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from game.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
